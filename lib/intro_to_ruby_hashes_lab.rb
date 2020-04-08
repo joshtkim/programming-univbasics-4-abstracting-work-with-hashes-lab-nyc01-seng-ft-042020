81eb3f69bab1d0ce2450af888ffdf63bc636511b
Joshua Kim <jykim92@gmail.com>
2020-04-08 21:16:16 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,5 +1,9 @@
 def my_hash_creator(key, value)
-  my_hash_creater(:name, "George")
+  hash = {
+    :name => "George"
+    :job => "GeorgE"
+  }
+  hash(:name, "George")
   # return a hash that includes the key and value parameters passed into this method
 end
 
